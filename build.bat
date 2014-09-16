1197055d81af55dd9c4877d89bdbc4f0dc1927a2
danmalcolm <danmalc@gmail.com>
2014-09-16 21:53:57 +0100

    Switch build to use MSBuild 12.0 - see http://msdn.microsoft.com/en-us/library/hh162058.aspx

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "build.targets" /p:Configuration=Release;Version=True /verbosity:detailed /filelogger
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "build.targets" /p:Configuration=Release;Version=True /verbosity:detailed /filelogger
 
