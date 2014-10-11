cb55e5f16759e9e35a5f9aad30c374e685a1d541
danmalcolm <danmalc@gmail.com>
2014-10-11 22:42:45 +0100

    UrlHelperExtensions comments

--- src/RezRouting.AspNetMvc/UrlGeneration/UrlHelperExtensions.cs
+++ src/RezRouting.AspNetMvc/UrlGeneration/UrlHelperExtensions.cs
@@ -7,29 +7,57 @@ using RezRouting.Utility;
 
 namespace RezRouting.AspNetMvc.UrlGeneration
 {
+    /// <summary>
+    /// Contains methods to build URLs based on routes created by RezRouting.ASPNetMvc
+    /// </summary>
     public static class UrlHelperExtensions
     {
         private static readonly ConcurrentDictionary<RouteCollection, RouteModelIndex> Indexes = new ConcurrentDictionary<RouteCollection, RouteModelIndex>();
         
         /// <summary>
-        /// Stores an index based on the supplied RouteCollection to enable quicker route URL generation. 
-        /// This method is designed to be called during application startup after all routes used by 
-        /// the application have been added to the collection. This optimisation can be used when there
-        /// is a guarantee that the routes in the collection will not change. 
+        /// Stores an index based on the supplied RouteCollection that can be used by 
+        /// UrlHelperExtensions for faster route URL generation. This method is designed 
+        /// to be called after all routes used by the application have been added to the
+        /// collection. This optimisation can be used when there is a guarantee that the
+        /// routes in the collection will not change after they have first been set up. 
         /// </summary>
         /// <param name="routes"></param>
         public static void IndexRoutes(RouteCollection routes)
         {
+            if (routes == null) throw new ArgumentNullException("routes");
+
             var index = new RouteModelIndex(routes);
             Indexes.TryAdd(routes, index);
         }
         
+        /// <summary>
+        /// Generates a fully qualified URL for a resource route based on the specified 
+        /// controller type, action and route values. Only routes created by RezRouting
+        /// are supported.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="controllerType"></param>
+        /// <param name="action"></param>
+        /// <param name="routeValues"></param>
+        /// <returns></returns>
         public static string ResourceUrl(this UrlHelper helper, Type controllerType, string action, object routeValues)
         {
             var rvd = routeValues != null ? new RouteValueDictionary(routeValues) : null;
             return helper.ResourceUrl(controllerType, action, rvd);
         }
 
+        /// <summary>
+        /// Generates a fully qualified URL for a resource route based on the specified 
+        /// controller type, action, route values, protocol and host name. Only routes 
+        /// created by RezRouting are supported.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="controllerType"></param>
+        /// <param name="action"></param>
+        /// <param name="routeValues"></param>
+        /// <param name="protocol"></param>
+        /// <param name="hostName"></param>
+        /// <returns></returns>
         public static string ResourceUrl(this UrlHelper helper, Type controllerType, string action, RouteValueDictionary routeValues, string protocol = null, string hostName = null)
         {
             const string modelKey = RouteDataTokenKeys.RouteModel;
