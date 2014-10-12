6b5d7868e558635fef2108b0a4cbb01b76b68f71
danmalcolm <danmalc@gmail.com>
2014-10-12 19:33:21 +0100

    Package ASP.Net MVC project in build

--- src/RezRouting.AspNetMvc/Properties/AssemblyInfo.cs
+++ src/RezRouting.AspNetMvc/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RezRouting.AspNetMvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A lightweight library for setting up routes based on a flexible resource-oriented URL structure in ASP.Net MVC applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dan Malcolm")]
 [assembly: AssemblyProduct("RezRouting.AspNetMvc")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -22,15 +21,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("37832539-2a6a-45e9-98fb-4b9443bc8bfb")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information specified separately in SharedAssemblyVersion.cs
--- src/RezRouting/Properties/AssemblyInfo.cs
+++ src/RezRouting/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RezRouting.Net")]
-[assembly: AssemblyDescription("A library that sets up ASP.Net MVC routes using resource-oriented URLs - similar to Ruby on Rails' built-in Resource Routing")]
+[assembly: AssemblyDescription("A lightweight library for setting up routes based on a flexible resource-oriented URL structure in ASP.Net MVC applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dan Malcolm")]
 [assembly: AssemblyProduct("RezRouting.Net")]
@@ -23,16 +23,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d655717d-bc33-402c-b32f-c5e12e6e070b")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha001")]
+// Version information specified separately in SharedAssemblyVersion.cs
