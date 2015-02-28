f5398a52fa217e634e4ba7ccc72ab9ba04056b51
danmalcolm <danmalc@gmail.com>
2015-02-28 21:21:02 +0000

    Additional UrlHelperExtension method overloads

--- src/RezRouting.AspNetMvc/UrlGeneration/UrlHelperExtensions.cs
+++ src/RezRouting.AspNetMvc/UrlGeneration/UrlHelperExtensions.cs
@@ -36,11 +36,39 @@ namespace RezRouting.AspNetMvc.UrlGeneration
         }
 
         /// <summary>
-        /// Resets indexes stored for RouteCollections indexed via IndexRoutes method
+        /// Resets all indexes based on RouteCollections
         /// </summary>
         public static void ClearIndexedRoutes()
         {
             Indexes.Clear();
+        }
+
+        /// <summary>
+        /// Generates a fully qualified URL for a resource route based on the specified 
+        /// controller type and action. Only routes created by RezRouting are supported.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static string ResourceUrl<TController>(this UrlHelper helper, string action)
+            where TController : Controller
+        {
+            return helper.ResourceUrl(typeof(TController), action, null);
+        }
+
+
+        /// <summary>
+        /// Generates a fully qualified URL for a resource route based on the specified 
+        /// controller type, action and route values. Only routes created by RezRouting
+        /// are supported.
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="controllerType"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static string ResourceUrl(this UrlHelper helper, Type controllerType, string action)
+        {
+            return helper.ResourceUrl(controllerType, action, null);
         }
 
         /// <summary>
